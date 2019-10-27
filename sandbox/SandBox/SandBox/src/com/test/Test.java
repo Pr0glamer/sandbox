8b3fe4f78d00a3ba00ca0207818d66010f2c98b7
Pr0glamer <nickolayladenko@gmail.com>
2019-10-27 14:47:43 +0200

    Added arraylst

--- sandbox/SandBox/SandBox/src/com/test/Test.java
+++ sandbox/SandBox/SandBox/src/com/test/Test.java
@@ -1,10 +1,15 @@
 package com.test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("Hello, GITO");
 		System.out.println("Hell, ooo");
+		List<String> lst = new ArrayList<String>();
+		lst.add("Wow");
 
 	}
 
